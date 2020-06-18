81edfede8d785c7da8964f61661c2ddb120cfe95
박지원 <jiwon0297@naver.com>
2020-06-18 15:30:33 +0900

    ff
    ff

---
+++ Europe/src/action_add/CommentWriteAction.java
@@ -0,0 +1,41 @@
+package action_add;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import action.Action2;
+import action.ActionForward;
+import dao.CommentDAO;
+import dto.CommentElementBean;
+
+public class CommentWriteAction implements Action2{
+	 @Override
+	    public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	        
+	        CommentDAO dao = CommentDAO.getInstance();
+	        CommentElementBean comment = new CommentElementBean();
+	        
+	        // 파리미터 값을 가져온다.
+	        int comment_board = Integer.parseInt(request.getParameter("comment_board"));
+	        String comment_id = request.getParameter("comment_id");
+	        String comment_content = request.getParameter("comment_content");
+
+	        comment.setNum(dao.getSeq());
+	        comment.setRef(comment_board);
+	        comment.setId(comment_id);
+	        comment.setContent(comment_content);
+	        
+	        boolean result = dao.insertComment(comment);
+	 
+	        if(result){
+	            response.setContentType("text/html;charset=euc-kr");
+	            PrintWriter out = response.getWriter();
+	            out.println("1");
+	            out.close();
+	        }
+	            
+	        return null;
+	    }
+}
--- Europe/src/dto/CommentElementBean.java
+++ Europe/src/dto/CommentElementBean.java
@@ -8,55 +8,74 @@ public class CommentElementBean implements java.io.Serializable {
 	   private String content;
 	   private Date reg;
 	   private int ref;
+	   private int comment_parent;
 	   
 	   // default constructor
 	   public CommentElementBean() {
-	      this(-1,null,null,null,-1);
+	      this(-1,null,null,null,-1,-1);
 	   }
 	   
-	   public CommentElementBean(int num, String id, String content, Date reg, int ref) {
+	   public CommentElementBean(int num, String id, String content, Date reg, int ref, int comment_parent) {
 		super();
 		this.num = num;
 		this.id = id;
 		this.content = content;
 		this.reg = reg;
 		this.ref = ref;
+		this.comment_parent = comment_parent;
+	}
+
+	@Override
+	public String toString() {
+		return "CommentElementBean [num=" + num + ", id=" + id + ", content=" + content + ", reg=" + reg + ", ref="
+				+ ref + ", comment_parent=" + comment_parent + "]";
 	}
 
 	public int getNum() {
 		return num;
 	}
+
 	public void setNum(int num) {
 		this.num = num;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	public Date getReg() {
 		return reg;
 	}
+
 	public void setReg(Date reg) {
 		this.reg = reg;
 	}
+
 	public int getRef() {
 		return ref;
 	}
+
 	public void setRef(int ref) {
 		this.ref = ref;
 	}
-	   
-	@Override
-	public String toString() {
-		return "CommentDTO [num=" + num + ", id=" + id + ", content=" + content + ", reg=" + reg + ", ref=" + ref + "]";
+
+	public int getComment_parent() {
+		return comment_parent;
 	}
 
+	public void setComment_parent(int comment_parent) {
+		this.comment_parent = comment_parent;
+	}
 }
